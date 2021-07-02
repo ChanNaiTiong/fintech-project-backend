5085c967e8c8417782cc887fb1b1509b24014a36
Chan Nai Tiong <ntchan2021@gmail.com>
2021-07-02 17:52:18 +0800

    End of Day 3

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 const product = require("./product");
 
+
+// create backend services (REST API) using express
+const express = require("express");
+const cors = require("cors");
+
+server = express();
+server.use(cors());
+
+// const bodyParser = require("body-parser");
+// server.use(bodyParser.json());
+server.use(express.json());
+
+// router = express.Router();
+
+// // add an entry to the router table - parameters - URI -> function
+// router.get("/welcome", (request, response) => {
+//     response.status(200).send("Welcome");
+// })
+
+// server.use(router);
+server.use(product.router);
+
+server.listen(3000, () => {
+    console.log("server is starting");
+});
+
+
+
 // product.get_all_products();
 
 // product.get_product_by_id(3); 
@@ -8,6 +36,6 @@ const product = require("./product");
 
 // product.delete_product_by_id(30);
 
-product.update_price_by_id(200,2500);
+// product.update_price_by_id(200,2500);
 
-product.get_all_products();
+// product.get_all_products();
--- product.js
+++ product.js
@@ -1,74 +1,150 @@
 const database = require("./database");
 
-function get_all_products() {
-    database.connection.query(
-        'select * from products',
-        (error, result) => {
+const express = require("express");
+
+router = express.Router();
+
+// add an entry to the router table - parameters - URI -> function
+router.get("/products/all", (request, response) => {
+    database.connection.query('select * from products', (error, result) => {
             if (error) {
                 console.log(error);
+                response.status(500).send("Error at backend");
             } else {
-                console.log(result);
+                response.status(200).send(result);
             }
-        }
-    )
-}
+    });
+});
 
-function get_product_by_id(id){
-    database.connection.query(`select * from products where id = ${id}`,
+// function get_all_products() {
+//     database.connection.query(
+//         'select * from products',
+//         (error, result) => {
+//             if (error) {
+//                 console.log(error);
+//             } else {
+//                 console.log(result);
+//             }
+//         }
+//     )
+// }
+
+router.get("/products/by-id", (request,response) => {
+    database.connection.query(`select * from products where id = ${request.query.id}`,
     (error, result) => {
         if (error) {
             console.log(error);
+            response.status(500).send("Error at backend");
         } else {
-            console.log(result);
+            response.status(200).send(result);
         }
     }
     )
-}
 
-function create_new_product(name, price) {
+})
+
+
+// function get_product_by_id(id){
+//     database.connection.query(`select * from products where id = ${id}`,
+//     (error, result) => {
+//         if (error) {
+//             console.log(error);
+//         } else {
+//             console.log(result);
+//         }
+//     }
+//     )
+// }
+
+router.post("/products/add", (request,response) => {
     database.connection.query(
-        `insert into products (id, market_price, name) values (200,${price},'${name}')`,
+        `insert into products (name, market_price) values ('${request.body.name}','${request.body.price}')`,
         (error, result) => {
             if (error) {
                 console.log(error);
+                response.status(500).send("Error at backend");
             } else {
-                console.log("Created successfully");
+                response.status(200).send("Created successfully");
             }
         }
     )
-}
+});
 
-function delete_product_by_id(id) {
+
+// function create_new_product(name, price) {
+//     database.connection.query(
+//         `insert into products (id, market_price, name) values (200,${price},'${name}')`,
+//         (error, result) => {
+//             if (error) {
+//                 console.log(error);
+//             } else {
+//                 console.log("Created successfully");
+//             }
+//         }
+//     )
+// }
+
+router.delete("/products/delete/by-id", (request,response)=> {
     database.connection.query(
-        `delete from products where id = ${id}`,
+        `delete from products where id = ${request.query.id}`,
         (error, result) => {
             if (error) {
                 console.log(error);
+                response.status(500).send("Error at backend");
             } else {
-                console.log("Deleted");
+                response.status(200).send("Deleted successfully");
             }
         }
     )
-}
+});
 
-function update_price_by_id(id, new_price) {
+// function delete_product_by_id(id) {
+//     database.connection.query(
+//         `delete from products where id = ${id}`,
+//         (error, result) => {
+//             if (error) {
+//                 console.log(error);
+//             } else {
+//                 console.log("Deleted");
+//             }
+//         }
+//     )
+// }
+
+router.put("/products/update/by-id", (request,response) => {
     database.connection.query(
-        `update products set market_price = ${new_price} where id = ${id}`,
+        `update products set name = '${request.body.new_name}', market_price = '${request.body.new_price}' where id = ${request.body.id}`,
         (error, result) => {
             if (error) {
+                response.status(500).send("Error at backend");
                 console.log(error);
             } else {
-                console.log("Update successfully");
+                response.status(200).send("Update successfully");
             }
         }
     )
-}
+});
+
+
+
+// function update_price_by_id(id, new_price) {
+//     database.connection.query(
+//         `update products set market_price = ${new_price} where id = ${id}`,
+//         (error, result) => {
+//             if (error) {
+//                 console.log(error);
+//             } else {
+//                 console.log("Update successfully");
+//             }
+//         }
+//     )
+// }
 
 module.exports = {
-    get_all_products,
-    get_product_by_id,    
-    create_new_product,  
-    delete_product_by_id,
-    update_price_by_id,
-  
+    // get_all_products,
+    // get_product_by_id,    
+    // create_new_product,  
+    // delete_product_by_id,
+    // update_price_by_id,
+    router,
 };
